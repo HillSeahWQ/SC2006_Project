ddc46cac02941d86f3cd60bfa08afbb58818bb00
Sachin Bhat <sachubhat17@gmail.com>
2022-11-04 14:47:12 +0800

    Update process.config.ts

--- Lab/Lab3/App_Skeleton/src/application/process.config.ts
+++ Lab/Lab3/App_Skeleton/src/application/process.config.ts
@@ -1,5 +1,7 @@
 import * as dotenv from 'dotenv';
 
+// @author: Keerthana Jayaraman Karthikeyan <KEER004@e.ntu.edu.sg>
+
 dotenv.config();
 
 const AnalyticsTracking = process.env.REACT_APP_GA_TRACKING_ID ?? '';
